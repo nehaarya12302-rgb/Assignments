aa8654794d66335ddf69ddebc1f76cebe52cf07a
nehaarya12302-rgb <nehaarya12302@gmail.com>
2025-10-07 14:32:03 +0530

    Add files via upload

---
+++ IP.C
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+for(int i=5; i>=1; i--) {          
+    for(int j=5; j>i; j--)          
+        printf(" ");
+    for(int k=1; k<=i; k++)         
+        printf("* ");
+    printf("\n");
+}
+}
